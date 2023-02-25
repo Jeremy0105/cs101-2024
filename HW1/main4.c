f948fb90312f9fe97f75a0f7edb7ef7e38f1b0d6
B1129009 <120005700+B1129009@users.noreply.github.com>
2023-02-26 03:43:58 +0800

    Create main4.c

---
+++ HW1/main4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[]= "-99acbc";
+    int alen = sizeof(a)/sizeof(char);
+    char b[alen];
+    for(int i=0;i<n;i++){
+        switch(a[i]){
+            case '1':
+            
+            break;
+            case '2':
+            k=k+2;
+            break;
+            case '3':
+            k=k+3;
+            break;
+            case '4':
+            k=k+4;
+            break;
+            case '5':
+            k=k+5;
+            break;
+            case '6':
+            k=k+6;
+            break;
+            case '7':
+            k=k+7;
+            break;
+            case '8':
+            k=k+8;
+            break;
+            case '9':
+            k=k+9;
+            break;
+            default:
+            break;
+        }
+    }
+    return 0;
+}
